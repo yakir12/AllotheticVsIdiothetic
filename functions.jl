e5c1a1ef13df27158cdd63cdb69f83a15fa93f19
Yakir Gagnon <12.yakir@gmail.com>
2024-09-06 14:56:01 +0200

    calibrate

--- functions.jl
+++ functions.jl
@@ -1,58 +1,72 @@
-tosecond(t::T) where {T <: Dates.TimePeriod} = t / convert(T, Dates.Second(1))
-tosecond(t::T) where {T <: Dates.TimeType} = tosecond(t - Time(0))
+tosecond(t::T) where {T <: TimeType} = tosecond(t - Time(0))
+tosecond(t::T) where {T <: TimePeriod} = t / convert(T, Dates.Second(1))
 
-# function get_spline(track)
-#     t, xy = track
-#     XY = reshape(collect(Iterators.flatten(xy)), 2, :)
-#     ParametricSpline(t, XY; s = 10000, k = 2)
-# end
+SimpTrack.track(file, start::Time, stop::Time; kwargs...) = track(file, tosecond(start), tosecond(stop); kwargs...)
 
-function smooth_rotate_split(track, POI)
-    t, xy = track
+function calibrate_smooth(clb, trk; s = 100, k = 2)
+    t, xy = trk
+    xy = clb.itform.(xy)
+    # smooth 
     XY = reshape(collect(Iterators.flatten(xy)), 2, :)
-    spl = ParametricSpline(t, XY; s = 10000, k = 2)
+    spl = ParametricSpline(t, XY; s, k)
+    return spl
+end
 
-    trans = Translation(-spl(t[1]))
-    p1 = SV(spl(t[1]))
-    p2 = SV(spl(POI))
+
+function rotate_trim(start, stop, POI, spl; cutoff = 50)
+    # resample
+    t = range(tosecond(start), tosecond(stop), round(Int, 30tosecond(stop - start))) # 30 fps
+    xy = SV.(spl.(t))
+
+    # center and rotate
+    trans = Translation(-xy[1])
+    p1 = xy[1]
+    i = findfirst(≥(tosecond(POI)), t)
+    p2 = xy[i]
     x, y = normalize(p2 - p1)
     θ = π/2 - atan(y, x)
     rot = recenter(LinearMap(Angle2d(θ)), p1)
-    t2xy = trans ∘ rot ∘ SV ∘ spl
+    cmp = trans ∘ rot
+    xy .= cmp.(xy)
 
-    before = t2xy.(range(t[1], POI, step = 1/25))
-    after = t2xy.(range(POI, t[end], step = 1/25))
-
-    return (before, after)
-end
-
-
-function plotit(name, start, stop, spline)
-    fig = Figure();
-    ax = Axis(fig[1,1], aspect=DataAspect())
-    ts = range(start, stop, 100)
-    xy = Point2f.(spline.(ts))
-    xy .-= xy[1]
-    lines!(ax, xy)
-    lines!(ax, Circle(zero(Point2f), 350), color=:black)
-    save("$name.pdf", fig)
-end
-
-function plotthem(names, starts, stops, splines)
-    fig = Figure();
-    ax = Axis(fig[1,1], aspect=DataAspect())
-    lins = []
-    for (name, start, stop, spline) in zip(names, starts, stops, splines)
-        ts = range(start, stop, 100)
-        xy = Point2f.(spline.(ts))
-        xy .-= xy[1]
-        push!(lins, lines!(ax, xy))
+    # trim
+    n = length(xy)
+    j = findfirst(>(cutoff) ∘ norm, xy)
+    if !isnothing(j)
+        @assert j ≥ i "The trimming cutoff must be larger than the POI, $i"
+        xy = xy[1:j - 1]
+        t = t[1:j - 1]
     end
-    lines!(ax, Circle(zero(Point2f), 350), color=:black)
-    Legend(fig[1,2], lins, names; nbanks=2)
-    # axislegend(ax, position = (1.2,1))
-    save("all.pdf", fig)
+
+    return (; t, i, xy)
 end
+
+# function plotit(name, start, stop, spline)
+#     fig = Figure();
+#     ax = Axis(fig[1,1], aspect=DataAspect())
+#     ts = range(start, stop, 100)
+#     xy = Point2f.(spline.(ts))
+#     xy .-= xy[1]
+#     lines!(ax, xy)
+#     lines!(ax, Circle(zero(Point2f), 350), color=:black)
+#     save("$name.pdf", fig)
+# end
+
+# function plotthem(names, starts, stops, splines)
+#     fig = Figure();
+#     ax = Axis(fig[1,1], aspect=DataAspect())
+#     lins = []
+#     for (name, start, stop, spline) in zip(names, starts, stops, splines)
+#         ts = range(start, stop, 100)
+#         xy = Point2f.(spline.(ts))
+#         xy .-= xy[1]
+#         push!(lins, lines!(ax, xy))
+#     end
+#     lines!(ax, Circle(zero(Point2f), 350), color=:black)
+#     Legend(fig[1,2], lins, names; nbanks=2)
+#     # axislegend(ax, position = (1.2,1))
+#     save("all.pdf", fig)
+# end
 
 function save_csv(name, track)
     t, xy = track
@@ -60,52 +74,36 @@ function save_csv(name, track)
     CSV.write("$name.csv", df)
 end
 
-function save_vid(name, file, track)
-    t, xy = track
-    vid = openvideo(file)
-    sz = out_frame_size(vid)
-    h = 200
-    fig = Figure(size=(h, round(Int, h * sz[2] / sz[1] / VideoIO.aspect_ratio(vid))), figure_padding=0)
-    ax = Axis(fig[1,1])
-    img = Observable(rotr90(read(vid)))
-    image!(ax, img)
-    y, x = xy[1]
-    point = Observable(Point2f(x, sz[2] - y))
-    scatter!(ax, point, marker='+', color=:red)
-    hidespines!(ax)
-    hidedecorations!(ax)
-    t₀ = gettime(vid)
-    t .+= t₀
-    seek(vid, t[1])
-    framerate = round(Int, 2length(t)/(t[end] - t[1]))
-    record(fig, "$name.mp4", zip(xy, vid); framerate) do (xy, frame)
-        img[] = rotr90(frame)
-        y, x = xy
-        point[] = Point2f(x, sz[2] - y)
-    end
-end
+# function save_vid(name, file, track)
+#     t, xy = track
+#     vid = openvideo(file)
+#     sz = out_frame_size(vid)
+#     h = 200
+#     fig = Figure(size=(h, round(Int, h * sz[2] / sz[1] / VideoIO.aspect_ratio(vid))), figure_padding=0)
+#     ax = Axis(fig[1,1])
+#     img = Observable(rotr90(read(vid)))
+#     image!(ax, img)
+#     y, x = xy[1]
+#     point = Observable(Point2f(x, sz[2] - y))
+#     scatter!(ax, point, marker='+', color=:red)
+#     hidespines!(ax)
+#     hidedecorations!(ax)
+#     t₀ = gettime(vid)
+#     t .+= t₀
+#     seek(vid, t[1])
+#     framerate = round(Int, 2length(t)/(t[end] - t[1]))
+#     record(fig, "$name.mp4", zip(xy, vid); framerate) do (xy, frame)
+#         img[] = rotr90(frame)
+#         y, x = xy
+#         point[] = Point2f(x, sz[2] - y)
+#     end
+# end
 
-function angle_between(oldu, newu)
-  x1, y1 = oldu
-  x2, y2 = newu
-  sign((x2 - x1)*(y2 + y1))*angle(oldu, newu)
-end
+cordlength(rotated) = cordlength(rotated.xy[rotated.i:end])
+cordlength(xy::Vector{SV}) = norm(diff([xy[1], xy[end]]))
 
-function cumulative_angle(xy)
-    δ = filter(!iszero ∘ sum, diff(xy))
-    # δ = filter(!iszero ∘ sum, diff(transfrm.(range(t1, t2, 1000))))
-    rad2deg(sum(splat(angle_between), partition(δ, 2, 1)))
-end
-
-# xy = [SV(0,0), SV(1,0.1), SV(2, -0.1)]
-# save("figure.pdf", scatter(xy, axis=(;aspect=DataAspect())))
-# δ = diff(xy)
-# rad2deg(sum(splat(angle_between), partition(δ, 2, 1)))
-
-
-cordlength(xy) = norm(diff([xy[1], xy[end]]))
-
-function curvelength(xy)
+curvelength(rotated) = curvelength(rotated.xy[rotated.i:end])
+function curvelength(xy::Vector{SV})
     p0 = xy[1]
     s = 0.0
     for p1 in xy[2:end]
@@ -115,20 +113,19 @@ function curvelength(xy)
     return s
 end
 
-# function straightness(transform, t1, t2)
-#     xy = transform.(range(t1, t2, 1000))
-#     cordlength(xy) / curvelength(xy)
-# end
+# tortuosity(rotated) = tortuosity(rotated.xy[rotated.i:end])
+# tortuosity(xy::Vector{SV}) = cordlength(xy) / curvelength(xy)
 
 
-# function rotate(spl, t2)
-#     t1 = first(spl.t)
-#     trans = Translation(-spl(t1))
-#     p1 = SV(spl(t1))
-#     p2 = SV(spl(t2))
-#     x, y = normalize(p2 - p1)
-#     θ = π/2 - atan(y, x)
-#     rot = recenter(LinearMap(Angle2d(θ)), p1)
-#     return trans ∘ rot
-# end
+function angle_between(oldu, newu)
+  x1, y1 = oldu
+  x2, y2 = newu
+  sign((x2 - x1)*(y2 + y1))*angle(oldu, newu)
+end
 
+cumulative_angle(rotated) = cumulative_angle(rotated.xy[rotated.i:end])
+function cumulative_angle(xy::Vector{SV})
+    δ = filter(!iszero ∘ sum, diff(xy))
+    # δ = filter(!iszero ∘ sum, diff(transfrm.(range(t1, t2, 1000))))
+    rad2deg(sum(splat(angle_between), partition(δ, 2, 1)))
+end
--- main.jl
+++ main.jl
@@ -1,54 +1,93 @@
 using SimpTrack
 using Dates, LinearAlgebra
-using CSV, DataFrames, Chain, DataFramesMeta
-using Dierckx, VideoIO, OhMyThreads
-using StaticArrays, CoordinateTransformations, Rotations, AngleBetweenVectors, IterTools
-using CairoMakie
+using CSV, DataFrames, Chain, DataFramesMeta, IterTools, AngleBetweenVectors
+using Dierckx, VideoIO, CameraCalibrations, OhMyThreads, ImageTransformations, ImageDraw, Colors
+using StaticArrays, CoordinateTransformations, Rotations
+# using CairoMakie
+using GLMakie
+using AlgebraOfGraphics
 
 const SV = SVector{2, Float64}
 
 include("functions.jl")
+include("calibrations.jl")
 
 data_path = "data"
-fps = 25 # frames per second
+
+# Calibrations
+
+calibrations = CSV.read(joinpath(data_path, "calib.csv"), DataFrame)
+
+# data quality checks
+@assert allunique(calibrations.calibration_id) "Calibration IDs not identical"
+for row in eachrow(calibrations)
+    @assert row.start < row.stop "start must occur before stop in calibration $(row.calibration_id)"
+    file = joinpath(data_path, row.file)
+    @assert isfile(file) "calibration file $file does not exist"
+    # @assert that the time stamps are within the period of the video
+end
+
+@rselect!(calibrations, :calibration_id, :calib = calib(joinpath(data_path, :file), :start, :stop, :extrinsic))
+
+# # save calibrations images for quality assesment
+# suspect_calibration = "20220304_calibration.mov 2nd"
+# check_calibration(calibrations, suspect_calibration)
 
 # Tracking
+df = CSV.read(joinpath(data_path, "runs.csv"), DataFrame)
 
-df = @chain joinpath(data_path, "runs.csv") begin
-    CSV.read(DataFrame)
-    @rtransform :name = first(splitext(:file))
-    @rtransform :file = realpath(joinpath("..", data_path, :path, :file))
-    transform!([:start, :POI, :stop] .=> ByRow(tosecond); renamecols = false)
+# data quality checks
+for row in eachrow(df)
+    @assert row.start ≤ row.POI ≤ row.stop "POI is not within the track in row $row"
+    @assert row.calibration_id ∈ calibrations.calibration_id "Calibration $calibration_id is missing from the calibrations file."
+    file = joinpath(data_path, row.path, row.file)
+    @assert isfile(file) "run file $file does not exist"
+    # @assert that the time stamps are within the period of the video
 end
 
-df.track = tmap(track, df.file, df.start, df.stop)
+df.track = tmap(track, joinpath.(data_path, df.path, df.file), df.start, df.stop)
 
-# # save mini video for debugging purposes
-tforeach(save_vid, df.name, df.file, df.track)
+# Combine the two
+leftjoin!(df, calibrations, on = :calibration_id)
+select!(df, Not(:calibration_id))
+
+# calibrate and smooth the track
+transform!(df, [:calib, :track] => ByRow(calibrate_smooth) => :spline)
+
+# # check the calibrated-smoothed tracks and videos
+# foreach(enumerate(eachrow(df))) do (i, row)
+#     t, xy = row.track
+#     save_vid(string(i), joinpath(data_path, row.path, row.file), row.calib.tform, t, xy, row.spline)
+# end
+
+# rotate and trim
+transform!(df, [:start, :stop, :POI, :spline] => ByRow(rotate_trim) => :rotated)
 
 
-# rotate them
+colors = Dict(zip(unique(df.condition), Makie.wong_colors()))
+@rtransform! df :color = colors[:condition]
 
-@chain df begin
-    transform!([:track, :POI] => ByRow(smooth_rotate_split) => [:before, :after])
-    transform!([:before, :after] .=> ByRow(cordlength))
-    transform!([:before, :after] .=> ByRow(curvelength))
-    transform!([:before_cordlength, :before_curvelength] => ByRow(/) => :before_straightness)
-    transform!([:after_cordlength, :after_curvelength] => ByRow(/) => :after_straightness)
-    transform!([:before, :after] .=> ByRow(cumulative_angle))
+
+fig = Figure()
+ax = Axis(fig[1,1], aspect=DataAspect(), xlabel = "X (cm)", ylabel = "Y (cm)")
+for r  in (30, 50)
+    lines!(ax, Circle(zero(Point2f), r), color=:gray, linewidth = 0.5)
+end
+for row in eachrow(df)
+    _, i, xy = row.rotated
+    lines!(ax, xy[1:i], color = :gray)
+    lines!(ax, xy[i:end], color = row.color, label = row.condition)
+end
+axislegend(ax, merge = true)
+save("fig.png", fig)
+
+
+tbl = @chain df begin
+    @transform :cordlength = cordlength.(:rotated)
+    @transform :curvelength = curvelength.(:rotated)
+    @transform :straightness = 1 .- :cordlength ./ :curvelength
+    @transform :cumulative_angle = cumulative_angle.(:rotated)
+    @select :condition :cordlength :curvelength :straightness :cumulative_angle
 end
 
-fig = Figure();
-axs = []
-for (i, (k, grp)) in enumerate(pairs(DataFrames.groupby(df, :condition)))
-    ax = Axis(fig[1,i], aspect=DataAspect(), title = k.condition)
-    push!(axs, ax)
-    for row in eachrow(grp)
-        lines!(ax, row.before, color = :gray)
-        lines!(ax, row.after, color = :black)
-    end
-end
-linkaxes!(axs...)
-save("figure.pdf", fig)
-
-CSV.write("summary.csv", select(df, Cols("name", "condition", r"cord", r"curve", r"straight", r"angle")))
+CSV.write("stats.csv", tbl)
