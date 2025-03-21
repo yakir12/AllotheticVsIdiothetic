05550bb664289328846e3945f733e721490413a2
Yakir Gagnon <12.yakir@gmail.com>
2025-03-21 15:40:30 +0100

    wip

--- indoors/analyse/main.jl
+++ indoors/analyse/main.jl
@@ -22,10 +22,16 @@ mkpath("figures")
 
 const results_dir = "../track_calibrate/tracks and calibrations"
 
+function combine_factors(light, induced, run)
+    induced = induced ? " induced" : ""
+    run = run > 1 ? " $run" : ""
+    string(light, induced, run)
+end
+
 runs = CSV.read(joinpath(results_dir, "runs.csv"), DataFrame)
 transform!(runs, :spontaneous_end => ByRow(!ismissing) => :dance_spontaneous)
-transform!(runs, [:light, :dance_induced] => ByRow((l, d) -> string(l, d ? " induced" : "")) => :condition)
-runs.condition .= categorical(runs.condition; levels = ["remain", "dark", "dark induced", "shift", "shift induced"], ordered = true)
+transform!(runs, [:light, :dance_induced, :at_run] => ByRow(combine_factors) => :condition)
+# runs.condition .= categorical(runs.condition; levels = ["remain", "dark", "dark induced", "shift", "shift induced"], ordered = true)
 
 calibs = CSV.read(joinpath(results_dir, "calibs.csv"), DataFrame)
 transform!(calibs, :calibration_id => ByRow(get_calibration) => :rectify)
@@ -53,10 +59,14 @@ transform!(runs, [:t, :spl] => ByRow(smooth_center) => :xyc)
 transform!(runs, [:t, :spl, :poi_index] => ByRow(get_smooth_center_poi_rotate) => :tform)
 
 ######################## Plots 
-my_renamer = renamer("remain" => "Remain", "dark" => "Dark", "dark induced" => "Dark induced")
 
 df = subset(runs, :light => ByRow(≠("shift")))
 
+# my_renamer = uppercasefirst ∘ string
+my_renamer = renamer("remain" => "Remain",
+                     "dark" => "Dark",
+                     "dark 10" => "Dark 10",
+                     "dark induced" => "Dark induced")
 
 transform!(groupby(df, :condition), eachindex => :n)
 combine(groupby(df, :condition), nrow).nrow
@@ -70,7 +80,7 @@ transform!(groupby(df1, :condition), eachindex => :n)
 subset!(df1, :n => ByRow(≤(10)))
 @assert all(==(10), combine(groupby(df1, :condition), nrow).nrow)
 
-fig = pregrouped(df1.xyc => first, df1.xyc => last, col = df1.condition => my_renamer, row = df1.at_run => nonnumeric) * visual(Lines) |> draw(; figure = (; size = (1200, 400)), axis=(aspect=DataAspect(), ))
+fig = pregrouped(df1.xyc => first, df1.xyc => last, col = df1.condition => my_renamer) * visual(Lines) |> draw(; figure = (; size = (1200, 400)), axis=(aspect=DataAspect(), ))
 for ax in fig.figure.content 
     if ax isa Axis
         for r  in (30, 50)
@@ -86,43 +96,11 @@ save(joinpath(output, "figure1.png"), fig)
 
 df1 = transform(df, [:t, :poi_index, :tform] => ByRow((t, i, f) -> f.(t[i:end])) => :xyp)
 
-fig = pregrouped(df1.xyp => first => "X (cm)", df1.xyp => last => "Y (cm)", col = df1.condition => my_renamer, row = df1.at_run => nonnumeric) * visual(Lines) |> draw(; figure = (; size = (1200, 300)), axis=(aspect=DataAspect(), limits = (nothing, (-5, nothing))))
+fig = pregrouped(df1.xyp => first => "X (cm)", df1.xyp => last => "Y (cm)", col = df1.condition => my_renamer) * visual(Lines) |> draw(; figure = (; size = (1200, 300)), axis=(aspect=DataAspect(), limits = (nothing, (-5, nothing))))
 
 save(joinpath(output, "figure2.png"), fig)
 
-# function sample_without(df1, nsample)
-#     l = floor(Int, minimum(norm ∘ last, df1.xyp))
-#     select!(df1, Cols(:condition, :xyp))
-#     transform!(groupby(df1, :condition), eachindex => :i)
-#     transform!(df1, :i => ByRow(i -> (i - 1) ÷ nsample + 1) => :group)
-#     transform!(groupby(df1, [:condition, :group]), nrow => :n)
-#     subset!(df1, :n => ByRow(==(nsample)))
-#     select!(df1, Not(:i, :n))
-#     transform!(groupby(df1, :condition), :group => maximum => :n)
-#     transform!(df1, [:group, :n] => ByRow((g, n) -> range(1, l, n)[g]) => :r)
-#     select!(df1, Not(:n))
-#     transform!(df1, [:xyp, :r] => ByRow((xy, r) -> get_exit_angle(xy, r)) => :θ)
-#     df2 = combine(groupby(df1, [:condition, :group]), :θ => mean_resultant_vector => :mean_resultant_vector, :r => first => :r)
-#     select!(df2, Not(:group))
-#     return df2
-# end
-#
-#
-#
-# df1 = transform(df, [:t, :poi_index, :tform] => ByRow((t, i, f) -> f.(t[i:end])) => :xyp);
-# df2 = vcat((sample_without(shuffle(df1), nsample) for nsample in 2:11)...);
-#
-# fig = data(df2) * mapping(:r => "Distance from POI (cm)", :mean_resultant_vector => "Mean resultant vector", color = :condition => my_renamer) * visual(Scatter; markersize = 25) |> draw(; axis = (; title = "After POI", limits = ((0, l+1), (0, 1))))
-#
-#
-#
-#
-#
-#
-# @assert all(==(nsample), combine(groupby(df1, [:condition, :group]), nrow).nrow)
-#
-# combine!(groupby(df1, [:condition, :group]), :xyp => ByRow(xy -> get_exit_angle(xy, r)) => :θ)
-#
+################################################### Figure 3
 
 df1 = transform(df, [:t, :poi_index, :tform] => ByRow((t, i, f) -> f.(t[i:end])) => :xyp)
 nr = 3
@@ -133,16 +111,36 @@ select!(df1, Cols(:condition, :θs))
 df1.r .= Ref(r)
 
 rl = range(extrema(r)..., 100)
-newdf = DataFrame(r = repeat(rl, outer = 3), condition = repeat(["remain", "dark", "dark induced"], inner = 100), mean_resultant_vector = zeros(300))
+conditions = levels(df1.condition)
+newdf = DataFrame(r = repeat(rl, outer = length(conditions)), condition = repeat(conditions, inner = 100), mean_resultant_vector = zeros(100length(conditions)))
 fm = @formula(mean_resultant_vector ~ r*condition)
-function bootstrap(df)
+function _bootstrap(df)
     n = nrow(df)
     df1 = flatten(df[sample(1:n, n), :], [:θs, :r])
     df2 = combine(groupby(df1, [:condition, :r]), :θs => mean_resultant_vector => :mean_resultant_vector)
-    m = BetaRegression.fit(BetaRegressionModel, fm, df2)
-    predict(m, newdf)
+    try
+        m = BetaRegression.fit(BetaRegressionModel, fm, df2)
+        predict(m, newdf)
+    catch ex
+        missing
+    end
 end
-c = stack(bootstrap(df1) for _ in 1:10_000)
+function bootstrap(df)
+    n = 10_000
+    ys = Vector{Vector{Float64}}(undef, n)
+    i = 0
+    while i < n
+        y = _bootstrap(df)
+        if ismissing(y)
+            continue
+        else
+            i += 1
+            ys[i] = y
+        end
+    end
+    return stack(ys)
+end
+c = bootstrap(df1)
 y = quantile.(skipmissing.(eachrow(c)), Ref([0.025, 0.5, 0.975]))
 newdf.lower .= getindex.(y, 1)
 newdf.mean_resultant_vector .= getindex.(y, 2)
@@ -151,6 +149,21 @@ newdf.upper .= getindex.(y, 3)
 fig = data(newdf) * mapping(:r, :mean_resultant_vector, lower = :lower, upper = :upper, color = :condition => my_renamer => "Light") * visual(LinesFill) |> draw(; axis = (; ylabel = "Mean resultant length", xlabel = "Radius (cm)", limits = ((0, l), (0, 1))))
 
 save(joinpath(output, "figure3.png"), fig)
+
+
+################################################## Figure 4
+
+df = subset(runs, :light => ByRow(==("shift")))
+
+my_renamer = uppercasefirst ∘ string
+
+
+
+
+
+
+
+
 
 sdjfdshflsjhfshdflhdlshflkdsa
 
