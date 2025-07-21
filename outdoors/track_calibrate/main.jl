0b39d15256d179e5ba5a282b1219d5559bd1c252
Yakir Gagnon <12.yakir@gmail.com>
2025-07-21 16:58:57 +0200

    wip

--- outdoors/track_calibrate/main.jl
+++ outdoors/track_calibrate/main.jl
@@ -3,8 +3,8 @@ using Fromage
 
 data_path = "/home/yakir/mnt/dacke_lab_data/Data/Elin/Project_AllotheticVsIdiothetic_outdoors"
 
-if isdir("tracks and calibrations")
-    rm("tracks and calibrations", recursive=true)
-end
+# if isdir("tracks and calibrations")
+#     rm("tracks and calibrations", recursive=true)
+# end
 
-main(data_path; delim = ';')
+main(data_path)
